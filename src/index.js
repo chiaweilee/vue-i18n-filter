91dde83ab3a23927c6648cff6fa1ab87a6b31cec
jv <ljw@live.jp>
2018-11-07 10:50:38 +0800

    feat: fix issue of $i18n is null

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ export default {
         const { filters } = this.$options
         const assets = name => {
           if (typeof filters[name] === 'undefined') {
-            filters[name] = (...arg) => this[`$${name}`] ? this[`$${name}`].apply(this, arg) : arg[0]
+            filters[name] = (...arg) => (this.$i18n && this[`$${name}`]) ? this[`$${name}`].apply(this, arg) : arg[0]
           }
         }
         assets('t')
