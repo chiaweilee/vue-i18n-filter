19b767f0e18ef250ab6aeba631213923f5fdef2a
jv <ljw@live.jp>
2019-03-17 01:59:45 +0800

    perf: 🔥 code

--- src/index.js
+++ src/index.js
@@ -3,21 +3,23 @@ export default {
     Vue.mixin({
       beforeCreate () {
         const { filters } = this.$options
-        const assets = name => {
+        const set = name => {
           if (typeof filters[name] === 'undefined') {
             filters[name] = (...arg) => (this.$i18n && this[`$${name}`]) ? this[`$${name}`].apply(this, arg) : arg[0]
+          } else {
+            console.warn(`[vue-i18n-filter] filter '${name}' already exists.`)
           }
         }
-        assets('t')
-        assets('tc')
-        assets('te')
+        set('t')
+        set('tc')
+        set('te')
       },
       beforeDestroy () {
-      const { filters } = this.$options
-      if (filters.t) { delete filters.t }
-      if (filters.te) { delete filters.te }
-      if (filters.tc) { delete filters.tc }
-    }
+        const { filters } = this.$options
+        delete filters.t
+        delete filters.te
+        delete filters.tc
+      }
     })
   }
 }
